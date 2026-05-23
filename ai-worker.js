37945028a952e3973ef67b8a9455b8db3b720e4e
안강현 <gimon0331@gmail.com>
2026-05-23 09:31:43 +0900

    Add AI worker

---
+++ ai-worker.js
@@ -0,0 +1,32 @@
+importScripts(
+  "./models/core.js",
+  "./models/greedy.js",
+  "./models/alpha-beta.js",
+  "./models/tactical.js"
+);
+
+const models = {
+  greedy: GreedyModel,
+  search: AlphaBetaModel,
+  tactical: TacticalModel
+};
+
+self.onmessage = event => {
+  const { id, board, model, depth } = event.data;
+  const selectedModel = models[model] || models.tactical;
+
+  try {
+    const move = selectedModel.findBestMove(cloneBoard(board), { depth });
+    self.postMessage({ id, move });
+  } catch (error) {
+    self.postMessage({
+      id,
+      move: null,
+      error: error instanceof Error ? error.message : String(error)
+    });
+  }
+};
+
+function cloneBoard(board) {
+  return board.map(row => row.slice());
+}
